ea4e463b8fe8bfba0c93b49554b4ceac9c5a06e2
Sumppy <kristof.kelle@gmail.com>
2022-03-02 21:34:23 +0100

    create subnets

--- vpc.tf
+++ vpc.tf
@@ -14,3 +14,17 @@ resource "aws_vpc" "terraform-CS" {
     "kubernetes.io/cluster/${var.cluster-name}" = "shared",
   })
 }
+
+resource "aws_subnet" "terraform-CS" {
+  count = 2
+
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  cidr_block              = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.terraform-CS.id
+
+  tags = tomap({
+    "Name"                                      = "terraform-eks-CS-node",
+    "kubernetes.io/cluster/${var.cluster-name}" = "shared",
+  })
+}
