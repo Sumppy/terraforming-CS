3aafa51f0b5cca93a1e294aa2a8eb5a4604fbf11
Sumppy <kristof.kelle@gmail.com>
2022-03-02 21:42:17 +0100

    create EC2 Security Group to allow networking traffic with EKS cluster

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -33,3 +33,30 @@ resource "aws_iam_role_policy_attachment" "terraform-CS-cluster-AmazonEKSVPCReso
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.terraform-CS-cluster.name
 }
+
+resource "aws_security_group" "terraform-CS-cluster" {
+  name        = "terraform-eks-terraform-CS-cluster"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = aws_vpc.terraform-CS.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-eks-terraform-CS"
+  }
+}
+
+resource "aws_security_group_rule" "terraform-CS-cluster-ingress-workstation-https" {
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow workstation to communicate with the cluster API Server"
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.terraform-CS-cluster.id
+  to_port           = 443
+  type              = "ingress"
+}
