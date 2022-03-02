d03182e09de57e9e9273f224a4c70845653f733c
Sumppy <kristof.kelle@gmail.com>
2022-03-02 21:41:09 +0100

    create IAM Role to allow EKS service to manage other AWS services

---
+++ eks-cluster.tf
@@ -0,0 +1,35 @@
+#
+# EKS Cluster Resources
+#  * IAM Role to allow EKS service to manage other AWS services
+#  * EC2 Security Group to allow networking traffic with EKS cluster
+#  * EKS Cluster
+#
+
+resource "aws_iam_role" "terraform-CS-cluster" {
+  name = "terraform-eks-terraform-CS-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "terraform-CS-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.terraform-CS-cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "terraform-CS-cluster-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.terraform-CS-cluster.name
+}
